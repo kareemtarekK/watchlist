45c7dad714d7d6a8b7069108ca1ec4ecab9907ff
kareemtarekK <engkareemtarek555@gmail.com>
2026-01-11 03:18:14 +0200

    login and create token and cookie

---
+++ src/config/token.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import jwt from "jsonwebtoken";
+
+console.log(process.env.NODE_ENV);
+
+const generateToken = (res, user) => {
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRESIN,
+  });
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production" ? true : false,
+    maxAge: 1000 * 60 * 60 * 24,
+    sameSite: "strict",
+  });
+  return token;
+};
+
+export { generateToken };
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,13 +1,59 @@
+import bcrypt from "bcryptjs";
+import "dotenv/config";
 import { prisma } from "./../db/prisma.js";
+import { generateToken } from "./../config/token.js";
+
 const register = async (req, res) => {
   const { name, email, password } = req.body;
+
+  const salt = await bcrypt.genSalt(10);
+  const passwordHashed = await bcrypt.hash(password, salt);
+
   const user = await prisma.user.create({
-    data: { name, email, password },
+    data: { name, email, password: passwordHashed },
   });
+
+  const token = generateToken(res, user);
+
   res.status(201).json({
     status: "success",
     data: {
+      user: { ...user, password: undefined },
+      token,
+    },
+  });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    return res.status(401).json({
+      status: "fail",
+      message: "email and password are required",
+    });
+
+  const user = await prisma.user.findUnique(email);
+  if (!user)
+    return res.status(401).json({
+      status: "fail",
+      message: "email or password is not correct",
+    });
+
+  const checkPassword = await bcrypt.compare(password, user.password);
+
+  if (!checkPassword)
+    return res.status(401).json({
+      status: "fail",
+      message: "email or password is not correct",
+    });
+
+  const token = generateToken(res, user);
+
+  res.status(200).json({
+    status: "success",
+    data: {
       user,
+      token,
     },
   });
 };
