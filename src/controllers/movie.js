d31f800a85b850631052d45ba8dcb34e0bd20c82
kareemtarekK <engkareemtarek555@gmail.com>
2026-01-11 18:05:51 +0200

    create movie & get all movies

---
+++ src/controllers/movie.js
@@ -0,0 +1,42 @@
+import { prisma } from "./../db/prisma.js";
+const createMovie = async (req, res) => {
+  const {
+    title,
+    overview,
+    releaseYear,
+    geners,
+    runtime,
+    posterUrl,
+    createdBy,
+  } = req.body;
+  const movie = await prisma.movie.create({
+    data: {
+      title,
+      overview,
+      releaseYear,
+      geners,
+      runtime,
+      posterUrl,
+      createdBy,
+    },
+  });
+  res.status(201).json({
+    status: "success",
+    data: {
+      movie,
+    },
+  });
+};
+
+const getAllMovies = async (req, res) => {
+  const movies = await prisma.movie.findMany({});
+  res.status(200).json({
+    status: "success",
+    length: movies.length,
+    data: {
+      movies,
+    },
+  });
+};
+
+export { createMovie, getAllMovies };
