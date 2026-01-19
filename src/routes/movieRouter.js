e5591f2bdd1c932c2ccb284a5fcbac5dffda85b4
kareemtarekK <engkareemtarek555@gmail.com>
2026-01-20 01:44:10 +0200

    add validate movie in router

--- src/routes/movieRouter.js
+++ src/routes/movieRouter.js
@@ -6,11 +6,19 @@ import {
   updateMovie,
   deleteMovie,
 } from "./../controllers/movie.js";
+
 import { protect } from "./../auth/protect.js";
+
+import { validateRequest } from "../validation/validateRequest.js";
+
+import movieValidationSchema from "../validation/validators/movieValidation.js";
 
 const movieRouter = express.Router();
 
-movieRouter.route("/").post(protect, createMovie).get(getAllMovies);
+movieRouter
+  .route("/")
+  .post(protect, validateRequest(movieValidationSchema), createMovie)
+  .get(getAllMovies);
 movieRouter
   .route("/:movieId")
   .get(getMovie)
