abf9f72cdd7f4b67338bdcf0733f76fedca41cea
kareemtarekK <engkareemtarek555@gmail.com>
2026-01-20 00:31:48 +0200

    fix movie router

--- src/routes/movieRouter.js
+++ src/routes/movieRouter.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { createMovie, getAllMovies } from "./../controllers/movie.js";
+import {
+  createMovie,
+  getAllMovies,
+  getMovie,
+  updateMovie,
+  deleteMovie,
+} from "./../controllers/movie.js";
 import { protect } from "./../auth/protect.js";
 
 const movieRouter = express.Router();
 
 movieRouter.route("/").post(createMovie).get(protect, getAllMovies);
-movieRouter.route("/:movieId").get().patch().delete();
+movieRouter
+  .route("/:movieId")
+  .get(getMovie)
+  .patch(updateMovie)
+  .delete(deleteMovie);
 
 export default movieRouter;
