79221679355e92ebfa64517e350325423315a7c8
kareemtarekK <engkareemtarek555@gmail.com>
2026-01-20 00:49:04 +0200

    authentication on user

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -7,12 +7,15 @@ import {
   updateUser,
   deleteUser,
 } from "./../controllers/user.js";
+import { protect } from "./../auth/protect.js";
 const userRouter = express.Router();
 
 userRouter.post("/register", register);
 userRouter.post("/login", login);
 
-userRouter.get("/", getallUsers);
+userRouter.get("/", protect, getallUsers);
+
+userRouter.use(protect);
 
 userRouter.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
 
