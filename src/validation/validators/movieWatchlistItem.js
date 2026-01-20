42af83370753f67ad5741add24fa42a9ec01cc14
kareemtarekK <engkareemtarek555@gmail.com>
2026-01-20 02:12:57 +0200

    validate watchlistitem

---
+++ src/validation/validators/movieWatchlistItem.js
@@ -0,0 +1,23 @@
+import { z } from "zod";
+const movieWatchlistItemSchema = z.object({
+  id: z.string().uuid(),
+  userId: z.string().uuid(),
+  movieId: z.string().uuid(),
+  rating: z.coerce
+    .number()
+    .int("rating must be interger")
+    .min(1, "rating must be between 1 and 10")
+    .max(10, "rating must be between 1 and 10")
+    .optional(),
+  status: z
+    .string()
+    .enum(["COMPLETED", "PLANNED", "WATCHING", "DROPPED"], {
+      error: () => ({
+        message: "status must be: COMPLETED PLANNED WATCHING DROPPED",
+      }),
+    })
+    .optional(),
+  notes: z.string().optional(),
+});
+
+export default movieWatchlistItemSchema;
